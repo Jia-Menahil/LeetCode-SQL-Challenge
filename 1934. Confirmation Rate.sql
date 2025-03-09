0294851d187179d59de4797820960bcd9d4f9a2e
Jia-Menahil <154730762+Jia-Menahil@users.noreply.github.com>
2025-03-09 14:27:06 +0500

    Create 1934. Confirmation Rate.sql

---
+++ 1934. Confirmation Rate.sql
@@ -0,0 +1,16 @@
+-- 1st method
+select s.user_id,
+    round(avg(if(action = "confirmed",1,0)),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on c.user_id = s.user_id
+group by s.user_id
+
+
+-- 2nd method(using case when)
+select s.user_id,
+    round(avg(case when c.action = 'confirmed' then 1 else 0 end),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on c.user_id = s.user_id
+group by s.user_id
